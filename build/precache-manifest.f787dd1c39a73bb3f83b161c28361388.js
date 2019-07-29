70f494f2609633c2feb3d3047d1fbef64036924d
Labais <reinitto@gmail.com>
2019-07-29 23:31:26 +0200

    add build folder

---
+++ build/precache-manifest.f787dd1c39a73bb3f83b161c28361388.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd07eda0f79275897ffbba7537da626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61433a0e5c1788d9b990",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "15d01be9aa2676e13cdf",
+    "url": "/static/js/2.d8131e92.chunk.js"
+  },
+  {
+    "revision": "61433a0e5c1788d9b990",
+    "url": "/static/js/main.5191febe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
